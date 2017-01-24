02d29cabae2a0e3494de5215ed60bd05fbc1aae5
PlanetEfficacy <jspevack@gmail.com>
2017-01-24 14:45:06 -0700

    Add store quantity update spec

--- spec/requests/api/v1/patch_products_spec.rb
+++ spec/requests/api/v1/patch_products_spec.rb
@@ -14,4 +14,18 @@ describe "PATCH /products/:id", type: :request do
       expect(product['warehouse_quantity']).to eq(2)
     end
   end
+
+  context "with update store body" do
+    it "returns the updated product as json" do
+      original_product = create :product, store_quantity: 1
+
+      patch "/api/v1/products/#{original_product.id}", { store_quantity: 2 }
+      product = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(product).to be_instance_of(Hash)
+      expect(product['id']).to eq(original_product.id)
+      expect(product['store_quantity']).to eq(2)
+    end
+  end
 end
