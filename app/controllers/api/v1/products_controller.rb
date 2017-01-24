f4bc2b20d25df198019f1cc83ef388017c5d660c
PlanetEfficacy <jspevack@gmail.com>
2017-01-24 14:20:09 -0700

    Add sort by price to api

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    render json: Product.all
+    if params[:sort_by] == "price"
+      products = Product.sort_by_price
+    else
+      products = Product.all
+    end
+    render json: products
   end
 end
--- spec/requests/api/v1/get_products_spec.rb
+++ spec/requests/api/v1/get_products_spec.rb
@@ -15,11 +15,32 @@ describe "GET /products", type: :request do
     expect(response).to be_success
     expect(products).to be_instance_of(Array)
     expect(products.count).to eq(2)
+    expect(products.first['id']).to eq(Product.first.id)
     expect(products.first['name']).to eq("Chocolate Bar")
     expect(products.first['price']).to eq("1.00")
     expect(products.first['warehouse_quantity']).to eq(1000)
     expect(products.first['store_quantity']).to eq(100)
     expect(products.first['expiration']).to eq(
       Product.first.expiration.strftime("%Y-%m-%d"))
+  end
+
+  context "request contains paramaters sort_by=price" do
+    it "returns all products sorted by price" do
+      product_1 = create :product, unit_price: 1
+      product_2 = create :product, unit_price: 2
+      product_3 = create :product, unit_price: 3, name: "A"
+      product_4 = create :product, unit_price: 3, name: "B"
+
+      get "/api/v1/products?sort_by=price"
+      products = JSON.parse(response.body)
+
+      expect(response).to be_success
+      # expect(response).to be_instance_of(Array)
+      expect(products.count).to eq(4)
+      expect(products.first['id']).to eq(product_3.id)
+      expect(products.second['id']).to eq(product_4.id)
+      expect(products.third['id']).to eq(product_2.id)
+      expect(products.last['id']).to eq(product_1.id)
+    end
   end
 end
