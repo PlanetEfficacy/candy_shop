125a197ffc00164d0854e9800a7ba3790bad7e15
PlanetEfficacy <jspevack@gmail.com>
2017-01-24 13:35:40 -0700

    Add test for sort by warehouse quantity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -22,11 +22,26 @@ describe Product, ".dollar_price" do
 end
 
 describe Product, ".sort_by_price" do
-  it "returns all the products ordered by unit price highest to lowest" do
+  it "returns all the products ordered by unit price high to low" do
     product_1 = create :product, unit_price: 1
     product_2 = create :product, unit_price: 2
     product_3 = create :product, unit_price: 3, name: "A"
     product_4 = create :product, unit_price: 3, name: "B"
+
+    expect(Product.sort_by_price.count).to eq(4)
+    expect(Product.sort_by_price.first).to eq(product_3)
+    expect(Product.sort_by_price.second).to eq(product_4)
+    expect(Product.sort_by_price.third).to eq(product_2)
+    expect(Product.sort_by_price.last).to eq(product_1)
+  end
+end
+
+describe Product, ".sort_by_warehouse" do
+  it "returns all the products ordered by warehouse quantity high to low" do
+    product_1 = create :product, warehouse_quantity: 1
+    product_2 = create :product, warehouse_quantity: 2
+    product_3 = create :product, warehouse_quantity: 3, name: "A"
+    product_4 = create :product, warehouse_quantity: 3, name: "B"
 
     expect(Product.sort_by_price.count).to eq(4)
     expect(Product.sort_by_price.first).to eq(product_3)
